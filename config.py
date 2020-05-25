e5d579b5a72f83501bcb48175e7603f3e03f90ba
Everton Silva <tomxds@live.com>
2020-05-25 18:00:07 -0300

    Configuração adicional para uso da aplicação.

--- config.py
+++ config.py
@@ -7,3 +7,25 @@ class Config():
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     TEMPLATE_FOLDER = os.path.join(ROOT_DIR, 'templates')
     APP = None
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    IP_HOST = 'localhost'
+    FORT_HOST = 8080
+    URL_MAIN = 'http://%s/%s' % (IP_HOST, FORT_HOST)  # http://localhost:8000
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://tiago.silva:de_m7jbG^wZ=@localhost:3306/dashboard_covid'
+
+
+class TestingConfig(Config):
+    pass
+
+
+app_config = {
+    'development': DevelopmentConfig(),
+    'testing': TestingConfig()
+}
+
+app_active = os.getenv('FLASK_ENV')
+if app_active is None:
+    app_active = 'development'
